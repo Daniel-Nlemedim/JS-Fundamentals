6759a19b12781ae2fac88a3056cf13fe0f41daff
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-05-26 08:08:27 +0100

    updated

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,11 +1,9 @@
-const args = process.argv;
+const args = process.argv.slice(2);
 
-if(args.length == 0){
+if(args.length === 0){
     console.log( args[0] || 'No arguments found');
-}else if(args.length == 1){
+}else if(args.length === 1){
     console.log(args[1] || 'Single argument found');
-}else if(args.length == 2 ){
-    console.log(args[2] || 'Multiple arguments found');
 }else {
     console.log('Arguments found')
 }
