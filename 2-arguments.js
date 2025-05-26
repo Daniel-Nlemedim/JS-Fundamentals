a843538e56952336f594729b3672f435c2f5888f
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-05-26 07:56:22 +0100

    updated

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,13 @@
 const args = process.argv;
 if(args.length == 0){
     console.log("No argument ")
-}else if(args.length == 1){
-    console.log('Argument found')
-}else if(args.length ==2 ){
+}else if(args.length == 2 ){
     console.log('Multiple arguments found')
-}else {
+}
+
+const args2 = process.argv
+if(args2.length == 0){
+    console.log("No argument ")
+}else{
     console.log('Arguments found')
 }
