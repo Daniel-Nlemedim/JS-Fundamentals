59bf7f0ff4b74cffb24a00e4ab1c5ec3914c904f
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-05-26 08:55:48 +0100

    updated

--- 4-concat.js
+++ 4-concat.js
@@ -1,5 +1,2 @@
-const concat = (a, b) => {
-    return a + b;
-}
-console.log(concat(undefined, ' is undefined'));
-console.log(concat('Python', ' is fun'));
+const words = 'Python' + ' is fun';
+console.log(words);
