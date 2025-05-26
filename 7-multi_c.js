291631c33ae61d122b349270f3a3563109c49d25
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-05-26 09:47:55 +0100

    updated

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
-const arg = ['x', 'C is fun']
-for (let i = 0; i < arg.length; i++) {
-    if(arg[i]!==parseInt(arg[i])){
-        console.log(`Missing number of occurrences ${i}`);
-    }else if(arg[i]===parseInt(arg[i]) && arg[i]!==0){
-        console.log(`Number of occurrences ${i}`);
-    }
-//   console.log(arg[i]);
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
