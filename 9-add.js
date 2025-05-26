5edb087fab006c0e34defa9f43486280507ab8ea
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-05-26 10:04:54 +0100

    updated

--- 9-add.js
+++ 9-add.js
@@ -3,5 +3,5 @@ const num2 = 'num2'; ;
 function add(a,b) {
     return a + b;
 }
-// console.log(add(num1, num2));
-console.log(add('1','3'));
+console.log(add(3, 1));
+// console.log(add('1','3'));
