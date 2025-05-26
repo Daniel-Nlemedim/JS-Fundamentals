06b3fd7a308b7a1fcf7dc6133ad8bc97b136977b
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-05-26 07:51:34 +0100

    changes

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,7 +3,7 @@ if(args.length == 0){
     console.log("No argument ")
 }else if(args.length == 1){
     console.log('Argument found')
-}else if(args.length> 1){
+}else if(args.length ==2 ){
     console.log('Multiple arguments found')
 }else {
     console.log('Arguments found')
