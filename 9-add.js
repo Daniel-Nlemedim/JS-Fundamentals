ad13dd39ca1266bf7409fd166eac93ef5114bff6
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-05-26 10:09:20 +0100

    update

--- 9-add.js
+++ 9-add.js
@@ -1,7 +1,6 @@
-const num1 = 'num1'; ;
-const num2 = 'num2'; ;    
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
 function add(a,b) {
     return a + b;
 }
-console.log(add(3, 1));
-// console.log(add('1','3'));
+console.log(add(num1, num2));
