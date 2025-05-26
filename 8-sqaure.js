895dcb310628d00cb73c15d7c338b22276159a8b
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-05-26 09:53:54 +0100

    updated

---
+++ 8-sqaure.js
@@ -0,0 +1,13 @@
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    let row = '';
+    for (let j = 0; j < x; j++) {
+      row += '*';
+    }
+    console.log(row);
+  }
+}
